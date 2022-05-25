0ccef3666d8d4da5651dbcfaffc4b134df4b5f0c
AronPelgrim <aron.pelgrim@hva.nl>
2022-05-25 10:57:13 +0200

    Made into node project

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const ejs = require('ejs')
+require('dotenv').config()
+
+app.set('view engine', 'ejs')
+
+app.set('views', './views/pages')
+
+app.use(express.static('public'))
+
+app.get('', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
