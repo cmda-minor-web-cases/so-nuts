f05d4693c7d566a60a1c57d50408b523f90a790e
AronPelgrim <aron.pelgrim@hva.nl>
2022-06-06 20:10:02 +0200

    Added ejs

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const ejs = require('ejs')
+const intakeData = require("./public/json/questionnaire.json")
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 require('dotenv').config()
+
 
 app.set('view engine', 'ejs')
 
@@ -15,9 +18,21 @@ app.get('/', (req, res) => {
 })
 
 app.get('/intake', (req, res) => {
-  res.render('intake')
+  fetchJson(`https://fhir.mibplatform.nl/api/Questionnaires/2`)
+  .then(function (jsonData) {
+    res.render('intake', {
+      data: jsonData
+    })
+  })
 })
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
+
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .then((body) => body.questions) 
+    .catch((error) => error)
+}
